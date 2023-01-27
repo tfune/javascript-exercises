d05a703d254dcc522fd502e944178a29c73d70f0
tfune <trevor.fune@gmail.com>
2023-01-27 13:36:03 -0800

    Complete exercises 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    return array.filter(arg => !args.includes(arg));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
