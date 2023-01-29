7cb8374257a75b7d576355b1392931898484ee9e
tfune <trevor.fune@gmail.com>
2023-01-29 11:10:48 -0800

    Complete exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let currentSum = 0;
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (typeof start != "number" || typeof end != "number") {
+        return 'ERROR';
+    } else if (start < end) {
+        for (let i = start; i <= end; i++) {
+            currentSum += i;
+        }
+        return currentSum;
+    } else if (start > end) {
+        for (let i = start; i >= end; i--) {
+            currentSum += i;
+        }
+        return currentSum;
+    }
 };
 
 // Do not edit below this line
